630f09a3a37ba6fb775bdc4703ca78f1171c521c
Илья Бондарчук <type1251@outlook.com>
2021-10-08 15:18:00 +1200

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class Point
+{
+	int x, y;
+public:
+	void setx(int newx)
+	{
+		x = newx;
+	}
+	void sety(int newy)
+	{
+		y = newy;
+	}
+	int getx()
+	{
+		return x;
+	}
+	int gety()
+	{
+		return y;
+	}
+	Point(int newx = 0, int newy = 0)
+	{
+		x = newx;
+		y = newy;
+		cout << "P\n";
+	}
+};
+class Circle
+{
+	int radius;
+	Point centr;
+public:
+	void setradius(int newradius)
+	{
+		radius = newradius;
+	}
+
+	int getradius()
+	{
+		return radius;
+	}
+	Circle(int newx, int newy) : centr(newx, newy) {
+
+	}
+};
+void main()
+{
+	Circle c(1, 1);
+	system("pause");
+}
