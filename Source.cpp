a0212f7078665c8bd73654f06de57f216b8346bb
Илья Бондарчук <type1251@outlook.com>
2021-10-10 19:46:55 +1200

    AlmostReady

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 class Point
 {
@@ -22,7 +23,7 @@ public:
 	}
 	void output()
 	{
-		cout << "x=" << x <<"\ny=" << y << endl;
+		cout << "x=" << x <<" y=" << y << endl;
 	}
 	Point(int newx = 0, int newy = 0)
 	{
@@ -34,6 +35,7 @@ class Circle
 {
 	int radius;
 	Point centr;
+	Point* p_centr;
 public:
 	void setradius(int newradius)
 	{
@@ -46,7 +48,7 @@ public:
 	}
 	void output()
 	{
-		cout << "radius=" << radius << "centr=";
+		cout << "radius=" << radius << " centr: ";
 		centr.output();
 		cout << endl;
 	}
@@ -55,14 +57,148 @@ public:
 		return 2 * 3.14 * radius;
 	}
 	int ploshad()
-		return 3.14 * radius * radius;
-	Circle(int newradius=1, int newx=0, int newy=0) : centr(newx, newy), radius(newradius)
+	{
+		return (3.14 * radius * radius);
+	}
+	Circle(int newradius=1, int newx=0, int newy=0) : p_centr(new Point(newx,newy))
 	{
 		radius = newradius;
 	}
+	~Circle()
+	{
+		delete p_centr;
+		cout << "Deleted dynamic array";
+	}
+	//Circle(int newradius=1, int newx=0, int newy=0) : centr(newx,newy)
+	//{
+	//	radius = newradius;
+	//}
 	
+};
+class Car
+{
+	string marka, colour, number;
+	int  probeg;
+public:
+	void setmarka(string newmarka)
+	{
+		marka = newmarka;
+	}
+	void setcolour(string newcolour)
+	{
+		colour = newcolour;
+	}
+	void setnumber(string newnumber)
+	{
+		number = newnumber;
+	}
+	void setprobeg(int newprobeg)
+	{
+		probeg = newprobeg;
+	}
+	string getmarka()
+	{
+		return marka;
+	}
+	string getcolour()
+	{
+		return colour;
+	}
+	string getnumber()
+	{
+		return number;
+	}
+	int getprobeg()
+	{
+		return probeg;
+	}
+	void information()
+	{
+		cout << "marka " << marka << " tsvet " << colour << " number " << number << " probeg " << probeg << endl;
+	}
+	Car(string newmarka = "Jiguul", string newcolour = "Red", string newnumber = "a000aa", int newprobeg=300000)
+	{
+		marka = newmarka;
+		colour = newcolour;
+		number = newnumber;
+		probeg = newprobeg;
+	}
+};
+class Man
+{
+	string fullname;
+	int age;
+	Car *vehicle;
+public:
+	void setname(string newfullname)
+	{
+		fullname = newfullname;
+	}
+	void setage(int newage)
+	{
+		age = newage;
+	}
+	void setcar(Car* newvehicle)
+	{
+		vehicle = newvehicle;
+	}
+	string getname()
+	{
+		return fullname;
+	}
+	int getage()
+	{
+		return age;
+	}
+	Car *getcar()
+	{
+		return vehicle;
+	}
+	void information()
+	{
+		cout << "Name= " << fullname << " Age= " << age;
+		if (vehicle == NULL)
+		{
+			cout << " Car= none";
+			cout << endl;
+		}
+		else
+		{
+			cout << " Car: ";
+			vehicle->information();
+			cout << endl;
+		}
+	}
+	Man()
+	{
+		fullname = "Petrov Petr Petrovich";
+		age = 30;
+		vehicle = NULL;
+	}
+	Man(string newfullname, int newage, Car *newvehicle)
+	{
+		fullname = newfullname;
+		age = newage;
+		vehicle = newvehicle;
+	}
 };
 void main()
 {
-	Circle c(1,1,1);
+	Circle c;
+	cout << "dlina= " << c.dlina() << endl << "Ploshad= " << c.ploshad() << endl;
+	c.output();
+	Car machina;
+	Car machina2("BMW", "Red", "b111aa", 40000);
+	machina.information();
+	machina2.information();
+	Car* stable = 0;
+	Man ivanov("Ivanov Petr Michailovich", 29, stable);
+	Car toyota("Toyota", "White", "a123bv", 255000);
+	ivanov.setcar(&toyota);
+	Man ivanova("Ivanova Klavdia Sergeevna", 25, &toyota);
+	toyota.setprobeg(260000);
+	ivanov.information();
+	ivanova.information();
+	Man noname;
+	noname.information();
 }
